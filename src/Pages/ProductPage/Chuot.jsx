9a36d2a97d3f10cee1aae932d914277711caf8db
itoshi-sae-prime <tukien159@gmail.com>
2025-04-18 11:36:08 +0700

    Update Chuot.jsx

--- src/Pages/ProductPage/Chuot.jsx
+++ src/Pages/ProductPage/Chuot.jsx
@@ -1,5 +1,5 @@
 import ProductList from "./ProductList";
 const ChuotPage = (props) => {
-    return <ProductList apiUrl={`products/chuot`} />;
+    return <ProductList apiUrl={`https://backend-kicap.onrender.com/api/products/chuot`} />;
 };
 export default ChuotPage;
