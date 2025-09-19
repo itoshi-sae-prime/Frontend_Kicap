6b32890c2bb182e01db1d4aa9a21c5058309226c
itoshi-sae-prime <tukien159@gmail.com>
2025-09-19 13:33:46 +0700

    Update slugify.js

--- src/utils/slugify.js
+++ src/utils/slugify.js
@@ -1,12 +1,15 @@
-const slugify = (text) =>
-    text
+const slugify = (text) => {
+    if (!text) return ""; // tránh lỗi khi null/undefined
+    return text
+        .toString()
         .toLowerCase()
         .normalize("NFD")
         .replace(/[\u0300-\u036f]/g, "")
-        .replace(/\s+/g, '-')
-        .replace(/[^\w\-]+/g, '')
-        .replace(/\-\-+/g, '-')
-        .replace(/^-+/, '')
-        .replace(/-+$/, '');
+        .replace(/\s+/g, "-")
+        .replace(/[^\w\-]+/g, "")
+        .replace(/\-\-+/g, "-")
+        .replace(/^-+/, "")
+        .replace(/-+$/, "");
+};
 
 export default slugify;
